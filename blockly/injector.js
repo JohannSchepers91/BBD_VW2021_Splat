6a56a58662f9a2ef3d9a072d7b16d55431427e21
Thabo <rachiditbose@gmail.com>
2021-07-29 17:10:14 +0200

    scrollbars

--- blockly/injector.js
+++ blockly/injector.js
@@ -1,4 +1,13 @@
+let options = {
+    move: {
+      scrollbars: {
+        horizontal: true,
+        vertical: true,
+      },
+      drag: true,
+    },
+    toolbox: document.getElementById('toolbox'),
+};
+
 // Injection (Adding a Blockly workspace)
-const workspace = Blockly.inject('blocklyDiv',{
-    toolbox: document.getElementById('toolbox')
-});
+const workspace = Blockly.inject('blocklyDiv',options);
