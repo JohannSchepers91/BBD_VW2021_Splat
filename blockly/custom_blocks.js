068c49eba34ece45f3da17f1627105d2ee81aa5e
Thabo <rachiditbose@gmail.com>
2021-07-29 13:55:39 +0200

    update colours

--- blockly/custom_blocks.js
+++ blockly/custom_blocks.js
@@ -26,186 +26,305 @@ Blockly.JavaScript['movement'] = function(block) {
     return 'new Command(Command.walk);';
 };*/
 Blockly.defineBlocksWithJsonArray([
-
-  {
-      type: "start",
-      message0: "Start %1",
-      args0: [
-          {type: "input_statement", name: "code"}
-      ]
+	{
+	"type": "start",
+	"message0": "START %1",
+	"args0": [
+	  {
+		"type": "input_statement",
+		"name": "code",
+		"align": "CENTRE"
+	  }
+	],
+	"inputsInline": false,
+	"colour": 270,
+	"tooltip": "Begin",
+	"helpUrl": ""
   },
-  
   {
-      type: "walk",
-      message0: "Walk",
-      previousStatement: null,
-      nextStatement: null
+	"type": "walk",
+	"message0": "walk",
+	"previousStatement": null,
+	"nextStatement": null,
+	"colour": 135,
+	"tooltip": "",
+	"helpUrl": ""
   },
-  
   {
-      type: "turn",
-  
-      message0: "Turn %1",
-      args0: [
-          {type: "field_dropdown", name: "value", options: [
-              ["Left", "Turn.left"],
-              ["Right", "Turn.right"],
-              ["Back", "Turn.back"]
-          ]}
-      ],
-  
-      previousStatement: null,
-      nextStatement: null
+	"type": "turn",
+	"message0": "Turn %1",
+	"args0": [
+	  {
+		"type": "field_dropdown",
+		"name": "turn",
+		"options": [
+		  [
+			"Left",
+			"Turn.left"
+		  ],
+		  [
+			"Right",
+			"Turn.right"
+		  ],
+		  [
+			"Back",
+			"Turn.back"
+		  ]
+		]
+	  }
+	],
+	"previousStatement": null,
+	"nextStatement": null,
+	"colour": 135,
+	"tooltip": "",
+	"helpUrl": ""
   },
-  
   {
-      type: "if_do_else",
-  
-      message0: "If %1",
-      args0: [
-          {type: "input_value", name: "condition", check: "CONDITION"}
-      ],
-  
-      message1: "do %1",
-      args1: [
-          {type: "input_statement", name: "do_code"}
-      ],
-  
-      message2: "else %1",
-      args2: [
-          {type: "input_statement", name: "else_code"}
-      ],
-  
-      previousStatement: null,
-      nextStatement: null
+	"type": "if_do_else",
+	"message0": "If %1 then %2 else %3",
+	"args0": [
+	  {
+		"type": "input_value",
+		"name": "condition"
+	  },
+	  {
+		"type": "input_statement",
+		"name": "do_code"
+	  },
+	  {
+		"type": "input_statement",
+		"name": "NAME"
+	  }
+	],
+	"previousStatement": null,
+	"nextStatement": null,
+	"colour": 0,
+	"tooltip": "",
+	"helpUrl": ""
   },
-  
   {
-      type: "repeat_until",
-  
-      message0: "Repeat Until %1",
-      args0: [
-          {type: "input_value", name: "condition", check: "CONDITION"}
-      ],
-      
-      message1: "do %1",
-      args1: [
-          {type: "input_statement", name: "code"}
-      ],
-  
-      previousStatement: null,
-      nextStatement: null,
-      colour: 120
+	"type": "repeat_until",
+	"message0": "Repeat Until %1 do %2",
+	"args0": [
+	  {
+		"type": "input_value",
+		"name": "condition"
+	  },
+	  {
+		"type": "input_statement",
+		"name": "NAME"
+	  }
+	],
+	"previousStatement": null,
+	"nextStatement": null,
+	"colour": 0,
+	"tooltip": "",
+	"helpUrl": ""
   },
-  
   {
-      type: "not",
-  
-      message0: "Not %1",
-      args0: [
-          {type: "input_value", name: "condition", check: "CONDITION"}
-      ],
-  
-      output: null
+	"type": "not",
+	"message0": "Not %1",
+	"args0": [
+	  {
+		"type": "input_value",
+		"name": "condition"
+	  }
+	],
+	"output": null,
+	"colour": 230,
+	"tooltip": "",
+	"helpUrl": ""
   },
-  
   {
-      type: "and",
-  
-      message0: "And %1 %2",
-      args0: [
-          {type: "input_value", name: "condition1", check: "CONDITION"},
-          {type: "input_value", name: "condition2", check: "CONDITION"}
-      ],
-      output: null
+	"type": "and",
+	"message0": "%1 And %2",
+	"args0": [
+	  {
+		"type": "input_value",
+		"name": "condition1"
+	  },
+	  {
+		"type": "input_value",
+		"name": "condition2"
+	  }
+	],
+	"inputsInline": true,
+	"output": null,
+	"colour": 230,
+	"tooltip": "",
+	"helpUrl": ""
   },
-  
   {
-      type: "or",
-  
-      message0: "Or %1 %2",
-      args0: [
-          {type: "input_value", name: "condition1", check: "CONDITION"},
-          {type: "input_value", name: "condition2", check: "CONDITION"}
-      ],
-      output: null
+	"type": "or",
+	"message0": "%1 Or %2",
+	"args0": [
+	  {
+		"type": "input_value",
+		"name": "condition1"
+	  },
+	  {
+		"type": "input_value",
+		"name": "condition2"
+	  }
+	],
+	"inputsInline": true,
+	"output": null,
+	"colour": 230,
+	"tooltip": "",
+	"helpUrl": ""
   },
-  
   {
-      type: "is_color",
-      message0: "Is Player Color %1",
-      args0: [
-          {type: "field_dropdown", name: "value", options:[
-              ["Red", "1"],
-              ["Yellow", "2"],
-              ["Blue", "4"],
-              ["Orange", "3"],
-              ["Purple", "5"],
-              ["Green", "6"],
-              ["Grey", "0"],
-              ["Goop", "7"]
-          ]}
-      ],
-  
-      output: "CONDITION"
+	"type": "is_color",
+	"message0": "Is player color %1 ?",
+	"args0": [
+	  {
+		"type": "field_dropdown",
+		"name": "value",
+		"options": [
+		  [
+			"Red",
+			"1"
+		  ],
+		  [
+			"Yellow",
+			"2"
+		  ],
+		  [
+			"Blue",
+			"4"
+		  ],
+		  [
+			"Orange",
+			"3"
+		  ],
+		  [
+			"Purple",
+			"5"
+		  ],
+		  [
+			"Green",
+			"6"
+		  ],
+		  [
+			"Grey",
+			"0"
+		  ],
+		  [
+			"Goop",
+			"7"
+		  ]
+		]
+	  }
+	],
+	"output": "CONDITION",
+	"colour": 315,
+	"tooltip": "",
+	"helpUrl": ""
   },
-  
   {
-      type: "is_tile_ahead",
-  
-      message0: "Is Tile Ahead %1",
-      args0: [
-          {type: "field_dropdown", name: "value", options: [
-              ["Empty", "Empty"],
-              ["Wall", "Wall"],
-              ["Splat", "Splat"],
-              ["Mixer", "Mixer"],
-              ["Bank", "Bank"],
-              ["Junction", "Junction"]
-          ]}
-      ],
-  
-      output: "CONDITION"
+	"type": "is_tile_ahead",
+	"message0": "Is Tile %1 Ahead?",
+	"args0": [
+	  {
+		"type": "field_dropdown",
+		"name": "value",
+		"options": [
+		  [
+			"Empty",
+			"Empty"
+		  ],
+		  [
+			"Wall",
+			"Wall"
+		  ],
+		  [
+			"Splat",
+			"Splat"
+		  ],
+		  [
+			"Mixer",
+			"Mixer"
+		  ],
+		  [
+			"Bank",
+			"Bank"
+		  ],
+		  [
+			"Junction",
+			"Junction"
+		  ]
+		]
+	  }
+	],
+	"output": "CONDITION",
+	"colour": 315,
+	"tooltip": "",
+	"helpUrl": ""
   },
-  
   {
-      type: "is_tile_current",
-  
-      message0: "Is Tile %1",
-      args0: [
-          {type: "field_dropdown", name: "value", options: [
-              ["Empty", "Empty"],
-              ["Wall", "Wall"],
-              ["Splat", "Splat"],
-              ["Mixer", "Mixer"],
-              ["Bank", "Bank"],
-              ["Junction", "Junction"]
-          ]}
-      ],
-  
-      output: "CONDITION"
+	"type": "is_tile_current",
+	"message0": "Is Current Tile %1 ?",
+	"args0": [
+	  {
+		"type": "field_dropdown",
+		"name": "value",
+		"options": [
+		  [
+			"Empty",
+			"Empty"
+		  ],
+		  [
+			"Wall",
+			"Wall"
+		  ],
+		  [
+			"Splat",
+			"Splat"
+		  ],
+		  [
+			"Mixer",
+			"Mixer"
+		  ],
+		  [
+			"Bank",
+			"Bank"
+		  ],
+		  [
+			"Junction",
+			"Junction"
+		  ]
+		]
+	  }
+	],
+	"output": "CONDITION",
+	"colour": 315,
+	"tooltip": "",
+	"helpUrl": ""
   },
-  
   {
-      type: "deposit",
-  
-      message0: "Deposit %1",
-      args0: [
-          {type: "field_input", name: "value", check: "Number"}
-      ],
-  
-      previousStatement: null,
-      nextStatement: null
+	"type": "deposit",
+	"message0": "Deposit %1",
+	"args0": [
+	  {
+		"type": "field_input",
+		"name": "value",
+		"text": ""
+	  }
+	],
+	"previousStatement": null,
+	"nextStatement": null,
+	"colour": 30,
+	"tooltip": "",
+	"helpUrl": ""
   },
-  
   {
-      type: "reached_end",
-  
-      message0: "Reached End",
-      output: "CONDITION"
-  }
-  
-  ]);
+	"type": "reached_end",
+	"message0": "Reached End",
+	"output": "CONDITION",
+	"colour": 30,
+	"tooltip": "",
+	"helpUrl": ""
+  }]);
   
   Blockly.JavaScript["start"] = function(block) {
   
